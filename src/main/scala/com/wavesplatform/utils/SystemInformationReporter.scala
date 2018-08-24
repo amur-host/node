60917a95f1d276f370652097c82e24f0b323bb95
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2018-08-24 16:29:13 +1000

    val orig = Seq

--- src/main/scala/com/wavesplatform/utils/SystemInformationReporter.scala
+++ src/main/scala/com/wavesplatform/utils/SystemInformationReporter.scala
@@ -7,7 +7,7 @@ object SystemInformationReporter extends ScorexLogging {
     val resolved = config.resolve()
     val configForLogs = {
       val orig = Seq(
-        "waves",
+        "amur",
         "metrics"
       ).foldLeft(ConfigFactory.empty()) { case (r, path) => r.withFallback(resolved.withOnlyPath(path)) }
 
